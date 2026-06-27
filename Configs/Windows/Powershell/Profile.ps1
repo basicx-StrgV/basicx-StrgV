31ec38ed63e6eddca9bc7c037c961b86b793aba5
Bjarne <basicx.strgv@gmail.com>
2026-06-27 16:09:02 +0200

    Updated the powershell profile

--- Configs/Windows/Powershell/Profile.ps1
+++ Configs/Windows/Powershell/Profile.ps1
@@ -5,3 +5,10 @@ Import-Module -Name Microsoft.WinGet.CommandNotFound
 #f45873b3-b655-43a6-b217-97c00aa0db58
 
 Import-Module posh-git
+
+$GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $false
+$GitPromptSettings.DefaultPromptPath.ForegroundColor = [ConsoleColor]::White
+$GitPromptSettings.DefaultPromptPrefix = '`n'
+$GitPromptSettings.DefaultPromptBeforeSuffix.Text = '`n'
+$GitPromptSettings.DefaultPromptSuffix = '>> '
+$GitPromptSettings.DefaultPromptSuffix.ForegroundColor = [ConsoleColor]::Magenta
